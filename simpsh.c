b65e49e991d3295a987c9404e1347f95520a813f
Nathan Chou <starrynte@gmail.com>
2016-01-12 19:13:15 -0800

    More refactoring, flush --verbose output.

--- simpsh.c
+++ simpsh.c
@@ -10,7 +10,7 @@
 int add_fd(int newfd, int **p, int *psize, int *pcapacity) {
 	if (*psize == *pcapacity) {
 		*pcapacity *= 2;
-		int *newp = realloc(*p, *pcapacity);
+		int *newp = realloc(*p, sizeof(int) * *pcapacity);
 		if (newp == NULL) {
 			fprintf(stderr, "Error calling realloc, proceeding to next option.\n");
 			return 1;
@@ -22,20 +22,10 @@ int add_fd(int newfd, int **p, int *psize, int *pcapacity) {
 	return 0;
 }
 
-// Pass pointers to cargs array, size, and capacity.
-int add_cargs(char *newcargs, char ***p, int *psize, int *pcapacity) {
-	if (*psize == *pcapacity) {
-		*pcapacity *= 2;
-		char **newp = realloc(*p, *pcapacity);
-		if (newp == NULL) {
-			fprintf(stderr, "Error calling realloc, proceeding to next option.\n");
-			return 1;
-		}
-		*p = newp; 
-	}
-	(*p)[*psize] = newcargs;
-	(*psize)++;
-	return 0;
+int is_option_argument(char* arg) {
+	// If arg is null, then we've reached the end of the argv array.
+	// Otherwise make sure arg is empty or does not start with "--".
+	return arg != NULL && (arg[0] == '\0' || !(arg[0] == '-' && arg[1] == '-'));
 }
 
 int main(int argc, char *argv[]) {
@@ -57,6 +47,27 @@ int main(int argc, char *argv[]) {
 	// Process option by option.
 	int opt;
 	while ((opt = getopt_long(argc, argv, "", options, NULL)) != -1) {
+		// Count number of arguments (anything not a long option) to this long option.
+		int nargs = 0;
+		// optarg is null when there were no arguments.
+		if (optarg != NULL) {
+			for (int i = optind - 1; is_option_argument(argv[i]); i++) {
+				nargs++;
+			}
+		}
+		// Check for Verbose for next option.
+		if (is_verbose == 1) {
+			// Index of the current long option.
+			int index = optind - (nargs == 0 ? 1 : 2);
+			printf("%s", argv[index]);
+			// Arguments.
+			for (int i = 1; i <= nargs; i++) {
+				printf(" %s", argv[index + i]);
+			}
+			printf("\n");
+			fflush(stdout);
+		}
+
 		switch (opt) {
 			case SIMPSH_RDONLY: case SIMPSH_WRONLY:
 			{
@@ -70,66 +81,35 @@ int main(int argc, char *argv[]) {
 					}
 					break;
 				}
-				int temp = add_fd(newfd, &fd, &fd_size, &fd_capacity);
-				if (temp == 1 && status < 1) {
+				if (add_fd(newfd, &fd, &fd_size, &fd_capacity) == 1 && status < 1) {
 					status = 1;
 				}
 				break;
 			}
 			case SIMPSH_COMMAND:
 			{
-				char *cmd = argv[optind + 2];
-
-				int index = optind - 1;
-				for (int i = 0; i < 4; i++) {
-					if (index >= argc ||
-						argv[index] == NULL ||
-						argv[index][0] == '\0' ||
-						(argv[index][0] == '-' && argv[index][1] == '-')) {
-						fprintf(stderr, "--command option requires at least 4 arguments.\n");
-						exit(1);
-					}
-					index++;
-				}
-				
-				int cargs_capacity = 8;
-				int cargs_size = 0;
-				char **cargs = malloc(sizeof(char*) * cargs_capacity);
-				// Whether or not parsing of cargs has been successful.
-				// We have to go through the arguments regardless to advance optind to the next option.
-				int cargs_failed = 0;
-				if (cargs == NULL) {
-					fprintf(stderr, "Error calling malloc for command %s, proceeding to next option.\n", cmd);
-					cargs_failed = 1;
-				}
-
-				// Parsing command arguments.
-				// Need to add the command itself as the first "argument".
-				index = optind + 2;
-				while (index < argc && argv[index][0] != '\0' && !(argv[index][0] == '-' && argv[index][1] == '-')) {
-					if (!cargs_failed) {
-						int temp = add_cargs(argv[index], &cargs, &cargs_size, &cargs_capacity);
-						if (temp == 1) {
-							cargs_failed = 1;
-						}
-					}
-					// Regardless of cargs_failed we should increment index until we find the next option.
-					index++;
-				}
-				// Save index of the 3 I/O file "arguments".
-				int io_index = optind - 1;
-				// Advance optind to next long option.
-				optind = index;
-				// If there was a problem storing cargs, abort.
-				if (cargs_failed) {
+				if (nargs < 4) {
+					fprintf(stderr, "--command option requires at least 4 arguments, proceeding to next option.\n");
 					if (status < 1) {
 						status = 1;
 					}
-					free(cargs);
 					break;
 				}
-				// execvp needs last element to be NULL.
-				add_cargs(NULL, &cargs, &cargs_size, &cargs_capacity);
+				
+				char *cmd = argv[optind + 2];
+				// (nargs - 3 + 1) arguments since we don't include the 3 I/O arguments but add a NULL to the end.
+				int cargs_size = nargs - 3 + 1;
+				char **cargs = malloc(sizeof(char*) * cargs_size);
+				if (cargs == NULL) {
+					fprintf(stderr, "Error calling malloc for command %s, proceeding to next option.\n", cmd);
+					if (status < 1) {
+						status = 1;
+					}
+					break;
+				}
+				// Copy arguments from argv, starting from the command itself, into cargs.
+				memcpy(cargs, argv + optind + 2, sizeof(char*) * (cargs_size - 1));
+				cargs[cargs_size - 1] = NULL;
 
 
 				pid_t child_pid;
@@ -146,10 +126,9 @@ int main(int argc, char *argv[]) {
 				// Execute command in the child process.
 				if(child_pid == 0) {
 					// Set command's standard io.		  
-					long *io = malloc(sizeof(long) * 3);
 					for (int j = 0; j < 3; j++) {
-						// Parse each file number.
-						io[j] = strtol(argv[io_index + j], NULL, 10);
+						// Parse each file number, converting to int.
+						int fdnum = strtol(argv[optind - 1 + j], NULL, 10);
 
 						char *msg;
 						switch(j) {
@@ -163,12 +142,12 @@ int main(int argc, char *argv[]) {
 								msg = "error";
 								break;
 						}  						
-						if ( (io[j] >= fd_size) || (io[j] < 0) ) {
+						if (fdnum >= fd_size || fdnum < 0) {
 							fprintf(stderr, "Standard %s for command %s refers to invalid file, aborting command.\n", msg, cmd);
 							exit(1);
 						}
-						if (dup2(fd[io[j]], j) == -1) {
-							fprintf(stderr, "Error redirecting standard %s for command %s to file %d, aborting command.\n", msg, cmd, io[j]);
+						if (dup2(fd[fdnum], j) == -1) {
+							fprintf(stderr, "Error redirecting standard %s for command %s to file %d, aborting command.\n", msg, cmd, fdnum);
 							exit(1);
 						}
 					}
@@ -187,35 +166,10 @@ int main(int argc, char *argv[]) {
 			}
 
 		}
-		// Check for Verbose.
-		if (is_verbose == 1) {
-			int index = optind;
-			while (index < argc &&
-				   !(argv[index] == NULL ||
-					 argv[index][0] == '\0' ||
-					 (argv[index][0] == '-' && argv[index][1] == '-'))) {
-				index++;
-			}
-			for(;;) {
-				if (index >= argc || argv[index] == NULL || argv[index][0] == '\0') {
-					break;
-				}
-				fprintf(stdout, "%s ", argv[index]);
-				index++;
-				if (index >= argc ||
-					argv[index] == NULL ||
-					argv[index][0] == '\0' ||
-					(argv[index][0] == '-' && argv[index][1] == '-')) {
-					fprintf(stdout, "%c", '\n');
-					break;
-				}
-				else {
-					fprintf(stdout, "%c", ' ');
-				}
-			}
-				
+		// Advance optind to the next long option, silently ignoring extraneous short options/arguments.
+		if (nargs > 1) {
+			optind += nargs - 1;
 		}
-
 	}
 		
 	// Close files/free.
